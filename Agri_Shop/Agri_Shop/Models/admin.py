dc029bfd8ba7faa3d3f7fd3b59a53c557829dd94
Chadi-Hammoud <chadihammoud96@gmail.com>
2023-05-04 02:50:53 +0300

    Admin Panel

--- Agri_Shop/Agri_Shop/Models/admin.py
+++ Agri_Shop/Agri_Shop/Models/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Models.models import *
 
 # Register your models here.
+admin.site.register(Farmer)
+admin.site.register(Client)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderDetails)
