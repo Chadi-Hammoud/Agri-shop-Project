8e71631edca4edb215bc7d4671b4a1f21866b645
albannakh <79979160+albannakh@users.noreply.github.com>
2023-05-04 20:30:53 +0300

    add client app

--- Agri_Shop/Agri_Shop/Agri_Shop/settings.py
+++ Agri_Shop/Agri_Shop/Agri_Shop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Models',
     'farmer',
+    'client',
         
 ]
 
---
+++ Agri_Shop/Agri_Shop/client/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def client_login(request):
+    return render(request,'client/singup.html',{"user": None})
