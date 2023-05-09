495b61f27cec0aac18b8039a7e951f6468922b66
Chadi-Hammoud <chadihammoud96@gmail.com>
2023-05-09 16:27:18 +0300

    login link

--- Agri_Shop/Agri_Shop/Accounts/views.py
+++ Agri_Shop/Agri_Shop/Accounts/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-    if request.method =='POST':
-        form =UserCreationForm(request.method)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-           form.save()
-           #LOG THE USER
-           return redirect('pages/index.html') 
-    else:  
+            form.save()
+            # redirect to success page or login page
+            return redirect('accounts:login') 
+    else:
         form = UserCreationForm()
-        return render(request, 'accounts/signup.html',{'form': form})
+    return render(request, 'Accounts/signup.html', {'form': form})
+    # if request.method =='POST':
+    #     form =UserCreationForm(request.POST)
+    #     if form.is_valid():
+    #        form.save()
+    #        #LOG THE USER
+    #        return redirect('Accounts:login') 
+    # else:  
+    #     form = UserCreationForm()
+    #     return render(request, 'accounts/signup.html',{'form': form})
 
 
 def login_view(request):
